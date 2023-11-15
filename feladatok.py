e7cd1fb3c8f521c15192a65275421de74c1582f3
Bottesz <botyaring@gmail.com>
2023-11-15 13:34:56 +0100

    prog tételek

---
+++ feladatok.py
@@ -0,0 +1,86 @@
+"""írj eljárást, mely a paraméterben kap két egész számot
+ezen két egész szám közötti
+páros számok összegét számolja ki az eljárás
+"""
+import math
+import random
+def szamok(szam1:int,szam2:int):
+    i: int = szam1
+    for i in range(szam1,szam2):
+        if i%2==0 :
+            print(i)
+            osszeg += i
+            i += 1
+
+    return osszeg
+
+def negativ():
+    i: int = 0
+    negativ_szamok = 0
+    while i < 10:
+        szam: int = math.floor(random.random(-10, 11))
+        if szam < 0:
+            negativ_szamok += 1
+        i+=1
+
+
+def negativ2 ():
+    #i: int = 0
+    db:int  = 0
+    for i in range = (0,20,1):
+        szam: int = math.floor(random.random(*21 + 10))
+        if szam < 0:
+            db += 1
+        i+=1
+
+import random
+import math
+def veletlen ():
+    osszeg: int = 0
+for i  in range(0, 10, 1 ):
+    szam: int = math.floor(random.random)*(12, (34-12))
+    osszeg += szam
+
+
+
+
+import random
+import math
+def vszamok():
+    for i in range (0,8,1):
+        szam:int=math.floor(random.random()*(50-20+20))
+        if szam>max :
+            max=szam
+
+
+def egesz_beker():
+    osszeg:int=0
+    for i in range (0,3,1):
+        szam:int = int(input(f"Kérem az {i+1}  egész számot")
+        osszeg += szam
+       
+        return osszeg/3
+
+
+def atlagszamolas():
+    i: int = 0
+    osszeg: int =0
+    db: int = 1
+    szam:int = int (input(f"Kérem az {i+1} egész számot"))
+    while szam!= szam
+        osszeg += szam
+        db += 1
+        1 += 1
+        szam:int =int(input(f"Kérem az {i+1}"))
+
+ 
+
+
+
+
+
+
+
+
+
+    
---
+++ main.py
@@ -0,0 +1,13 @@
+import feladatok
+feladatok.szamok(9,16)
+
+print(f"Párom számok összege: {osszeg}")
+
+"""
+Írj függvényt amely generál 10 db véletlen számot
+-10 és 10 között
+Számold meg hány negatív szám van közötte
+A visszatérési érték a negatív számok száma
+"""
+negativ_db:int = feladatok.negativ()
+print(f"negatív számok száma:{negativ_db}")
